01d6f4efae6fc0a947df4369e24f99bb7a2b2364
Fomichevsj <fomichevsj@gmail.com>
2017-06-22 00:18:23 +0300

    Update FFT.java

--- IdeaProjects/untitled/FCHVproject/src/FFT.java
+++ IdeaProjects/untitled/FCHVproject/src/FFT.java
@@ -61,73 +61,6 @@ public class FFT {
         return y;
     }
 
-
-    // compute the inverse FFT of x[], assuming its length is a power of 2
-    public static Complex[] ifft(Complex[] x) {
-        int N = x.length;
-        Complex[] y = new Complex[N];
-
-        // take conjugate
-        for (int i = 0; i < N; i++) {
-            y[i] = x[i].conjugate();
-        }
-
-        // compute forward FFT
-        y = fft(y);
-
-        // take conjugate again
-        for (int i = 0; i < N; i++) {
-            y[i] = y[i].conjugate();
-        }
-
-        // divide by N
-        for (int i = 0; i < N; i++) {
-            y[i] = y[i].times(1.0 / N);
-        }
-
-        return y;
-
-    }
-
-    // compute the circular convolution of x and y
-    public static Complex[] cconvolve(Complex[] x, Complex[] y) {
-
-        // should probably pad x and y with 0s so that they have same length
-        // and are powers of 2
-        if (x.length != y.length) { throw new RuntimeException("Dimensions don't agree"); }
-
-        int N = x.length;
-
-        // compute FFT of each sequence
-        Complex[] a = fft(x);
-        Complex[] b = fft(y);
-
-        // point-wise multiply
-        Complex[] c = new Complex[N];
-        for (int i = 0; i < N; i++) {
-            c[i] = a[i].times(b[i]);
-        }
-
-        // compute inverse FFT
-        return ifft(c);
-    }
-
-
-    // compute the linear convolution of x and y
-    public static Complex[] convolve(Complex[] x, Complex[] y) {
-        Complex ZERO = new Complex(0, 0);
-
-        Complex[] a = new Complex[2*x.length];
-        for (int i = 0;        i <   x.length; i++) a[i] = x[i];
-        for (int i = x.length; i < 2*x.length; i++) a[i] = ZERO;
-
-        Complex[] b = new Complex[2*y.length];
-        for (int i = 0;        i <   y.length; i++) b[i] = y[i];
-        for (int i = y.length; i < 2*y.length; i++) b[i] = ZERO;
-
-        return cconvolve(a, b);
-    }
-
     // display an array of Complex numbers to standard output
     public static void show(Complex[] x, String title) {
         System.out.println(title);
@@ -135,7 +68,7 @@ public class FFT {
         for (int i = 0; i < x.length; i++) {
             System.out.print(x[i].re());
             System.out.print("  + ");
-            System.out.println(x[i].im());
+            System.out.println(x[i].im()+ "I");
         }
         System.out.println();
     }
@@ -163,17 +96,7 @@ public class FFT {
         Complex[] y = fft(x);
         show(y, "y = fft(x)");
 
-        // take inverse FFT
-        Complex[] z = ifft(y);
-        show(z, "z = ifft(y)");
-
-        // circular convolution of x with itself
-        Complex[] c = cconvolve(x, x);
-        show(c, "c = cconvolve(x, x)");
-
-        // linear convolution of x with itself
-        Complex[] d = convolve(x, x);
-        show(d, "d = convolve(x, x)");
+        
     }
 
 }
