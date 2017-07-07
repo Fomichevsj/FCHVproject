84157be15fe0504d05bc293be1cc7808245892ef
safomichev <fomichevsj@gmail.com>
2017-07-07 20:38:37 +0300

    New Project

--- IdeaProjects/Learn/src/test/java/com/FomichevLTD/Stepick/AppTestJunit5.java
+++ IdeaProjects/Learn/src/test/java/com/FomichevLTD/Stepick/AppTestJunit5.java
@@ -3,6 +3,14 @@ package com.FomichevLTD.Stepick;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import java.net.URL;
 
 /**
  * Created by safomichev on 30.06.2017.
@@ -11,29 +19,48 @@ import org.junit.Test;
 public class AppTestJunit5 {
 
     private static CustomLogger logger;
-
-
+    private static WebDriver driver;
+    @FindBy(xpath = ".//input[@value=\"Мне повезёт\"]")
+    private WebElement luckky;
 
 
     @Test
     public void firstTestMethod() {
+        // Navigate to a web page
+        driver.navigate().to("https://www.google.ru");
+        try{
+            Thread.sleep(5000);
+        }
+        catch (Exception e) {
+            logger.debug("Словиили обишку");
+        }
+        luckky.click();
+        //driver.findElement(By.xpath(".//input[@value=\"Мне повезёт!\"])"));
+        try{
+            Thread.sleep(5000);
+        }
+        catch (Exception e) {
+            logger.debug("Словиили обишку");
+        }
 
 
 
 
 
 
-
-    }
+}
 
     @BeforeClass()
     public static void setUp() {
         logger = new CustomLogger();
         logger.debug("Test ClassLogger Created");
+        driver = new FirefoxDriver();
+
     }
 
     @AfterClass()
     public static void tearDown() {
         logger.debug("TearDown Test");
+        driver.quit();
     }
 }
